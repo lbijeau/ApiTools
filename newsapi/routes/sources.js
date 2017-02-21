95b99d4176f5cbf719ad7a14ef767319ba9b6fc3
LUC BIJEAU <lbijeau@gmail.com>
2017-02-20 22:01:41 -0500

    Add ability to configure countries, categories and languages via server side config file. Right now use defaults.  Also using ‘unfriendly’ names in some cases (short country codes). Beef it up later with friendly names, etc.

--- newsapi/routes/sources.js
+++ newsapi/routes/sources.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
+
+// Read configuration for news source options
+var sourceConfig = config.get("SourceConfig");
 
 const querystring = require('querystring');
 var Client = require('node-rest-client').Client;
@@ -15,20 +19,26 @@ router.get('/', function(req, res, next) {
         parameters: req.query, // query parameter substitution vars 
         //headers: { "test-header": "client-api" } // request headers 
     };    
-    
+        
     client.get("https://newsapi.org/v1/sources", args, function (data, response) {
+                
         // parsed response body as js object 
         if (req.accepts('text/html')) {
             res.send(data);
+            //next();
             return;
         } else if (req.accepts('application/json')) {
             res.send(data);
+            //next();
             return;
         }
 
         res.send("Must specify content type in request: application/json");
     });
     
+}).get("/config", function(req, res, next){
+  res.send(sourceConfig);
+  //next();
 });
 
 module.exports = router;
